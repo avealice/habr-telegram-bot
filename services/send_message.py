9acb1ef67be7758bc3e25493db6c2b602369b1d4
avealice <minisol@bk.ru>
2024-03-01 21:00:24 +0300

    fix send_message

--- services/send_message.py
+++ services/send_message.py
@@ -18,44 +18,20 @@ headers = {
 db = DataBase("database/db2.db")
 
 async def send_message(bot: Bot, period: str):
-    # users_id = db.get_users()
-
-    # for user_id in users_id:
-    #     subscriptions = db.get_subscribtions(user_id[0])
-    #     if subscriptions and period == 'daily':
-    #         await bot.send_message(user_id[0], f"ТОП СТАТЬИ ЭТОГО ДНЯ")
-    #     weekly_pages = {}
-    #     for subscription in subscriptions:
-    #         page = get_page(subscription[0], period)
-    #         try:
-    #             if page and period == "daily":
-    #                 await bot.send_message(user_id[0], f'Хаб <b>"{hubs[subscription[0]]}"</b>:\n\n{page}')
-    #                 await asyncio.sleep(7)
-    #             elif page and period == "weekly":
-    #                 weekly_pages.setdefault(subscription, page)
-    #             else:
-    #                 await bot.send_message(user_id[0], f'Статья дня в хабе <b>"{hubs[subscription[0]]}"</b> сегодня отсутствует 😔')
-    #         except Exception as e:
-    #             print(f'Юзер заблокировал бота, error - {e}')
-    #     if len(weekly_pages):
-    #         await bot.send_message(user_id[0], f"ТОП СТАТЬИ ЭТОЙ НЕДЕЛИ")
-    #         for hub, url in weekly_pages.items():
-    #             await bot.send_message(user_id[0], f'Хаб <b>"{hubs[hub[0]]}"</b>:\n\n{url}')
-    #             await asyncio.sleep(7)
     users_id = db.get_users()
     pages = await parse_habr(period)
 
     for user_id in users_id:
         if period == "Daily":
-            await bot.send_message(user_id[0], f"ТОП СТАТЬИ ЭТОГО ДНЯ")
+            await bot.send_message(user_id[0], "ТОП СТАТЬИ ЭТОГО ДНЯ")
         else:
-            await bot.send_message(user_id[0], f"ТОП СТАТЬИ ЭТОЙ НЕДЕЛИ")
+            await bot.send_message(user_id[0], "ТОП СТАТЬИ ЭТОЙ НЕДЕЛИ")
         subscriptions = db.get_subscribtions(user_id[0])
         for subscription in subscriptions:
             if pages[subscription[0]]:
-                await bot.send_message(user_id[0], f'Хаб <b>{subscription[0]}</b>:\n\n{pages[subscription[0]]}')
+                articles_str = '\n'.join(map(str, pages[subscription[0]]))
+                await bot.send_message(user_id[0], f'Хаб <b>{subscription[0]}</b>:\n\n{articles_str}')
                 await asyncio.sleep(5)
-
 
 async def parse_habr(period: str) -> dict:
     pages = {hub: set() for hub in hubs}
